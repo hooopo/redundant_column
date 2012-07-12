6b1ec7feb3c5a3d29fb209fd2e9ebaabc2c468be
Hooopo <hoooopo@gmail.com>
2012-07-13 01:21:31 +0800

    rewrite has_many macro

--- lib/redundant_column.rb
+++ lib/redundant_column.rb
@@ -1,34 +1,59 @@
 require "redundant_column/version"
 
 module RedundantColumn
-  def redundant_column(name, target_model, options = {}) 
-    set_callback :update, :before, CallbackBuilder.new(name, target_model, options)
-  end
+  module HasMany
+    class CallbackBuilder
+      attr_reader :name, :target_name, :association_name
 
-  class CallbackBuilder
-    attr_reader :name, :target_model, :target_name, :association_name
+      def initialize(name, target_name, association_name)
+        @name = name
+        @target_name =  target_name 
+        @association_name = association_name
+      end
 
-    def initialize(name, target_model, options = {})
-      @name = name
-      @target_model = target_model
-      @target_name =  options[:target_name] 
-      @association_name = options[:association_name] || "#{target_model.name.downcase.pluralize}"
+      def before_update(caller) 
+        if caller.send("#{name}_changed?")
+          if caller.respond_to?(association_name)
+            new_column_value = caller.send(name)
+            update_sql = new_column_value.is_a?(String) ? %Q|#{target_name} = '#{new_column_value.gsub("'", "\\'")}'| : %Q|#{target_name} = #{new_column_value}|
+            caller.send(association_name).update_all(update_sql)
+          else
+            STDERR.puts "You must set association in #{caller.class} model:"
+            STDERR.puts "has_many :#{association_name}, ..."
+          end
+        end
+      end
     end
 
-    def before_update(caller) 
-      if caller.send("#{name}_changed?")
-        if caller.respond_to?(association_name)
-          new_column_value = caller.send(name)
-          target_name ||= "#{caller.class.name.downcase}_#{name}"
-          update_sql = new_column_value.is_a?(String) ? %Q|#{target_name} = '#{new_column_value.gsub("'", "\\'")}'| : %Q|#{target_name} = #{new_column_value}|
-          caller.send(association_name).update_all(update_sql)
+
+    def self.included(base)
+      base.valid_options += [:redundant_column]
+    end
+
+    def build
+      reflection = super
+      configure_dependency
+      configure_update_redundant_column_callback
+      reflection
+    end
+
+    private
+
+    def configure_update_redundant_column_callback
+      if columns = options[:redundant_column]
+        if columns.is_a?(Array)
+          columns.each do |column|
+            model.set_callback :update, :before, CallbackBuilder.new(column, model.name.downcase + "_#{column}", self.name)
+          end
+        elsif columns.is_a?(Hash)
+          columns.each do |column, target_name|
+            model.set_callback :update, :before, CallbackBuilder.new(column, target_name, self.name)
+          end
         else
-          STDERR.puts "You must set association in #{caller.class} model:"
-          STDERR.puts "has_many :#{association_name}, ..."
+          model.set_callback :update, :before, CallbackBuilder.new(column, model.name.downcase + "_#{columns}", self.name)
         end
       end
     end
   end
 end
-
-ActiveRecord::Base.send :extend, RedundantColumn
+ActiveRecord::Associations::Builder::HasMany.send(:include, RedundantColumn::HasMany)
